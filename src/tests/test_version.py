2261628cac8ca1bc0e92c910c4b3c28cbc3c0dc8
Sam <sam.vanhoutte@marchitec.be>
2020-04-07 09:33:29 +0200

    update tests

--- src/tests/test_version.py
+++ src/tests/test_version.py
@@ -8,7 +8,7 @@ def test_base():
     assert (2+3) == 5
 
 def test_version_out():
-    assert (vr.output() == '0.0.1')
+    assert (vr.output() == '0.0.2')
 
 def test_version_variable():
-    assert (arcus.__version__ == '0.0.1')
+    assert (arcus.__version__ == '0.0.2')
