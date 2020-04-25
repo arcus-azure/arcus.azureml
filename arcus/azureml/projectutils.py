6e69456868f658fd02337072d533b081c22f59bd
Sriram Narayanan(Codit) <sriram.narayanan@codit.eu>
2020-04-25 18:45:05 +0200

    Added project utils

--- arcus/azureml/projectutils.py
+++ arcus/azureml/projectutils.py
@@ -1,16 +1,22 @@
+'''
+The projectutils module provides common operations related to data/ai projects
+'''
+
 import os
 
 def initProjectStructure():
-    '''Initialize the folder strucure for the Data/AI Project that facilitates Azure MLOps 
+    '''Creates the folders to start a the Data/AI Project that facilitates Azure MLOps
+       Skips folder creation if folder already exists. 
     Args:
         No Arguments
     Returns: 
         Void in case of success and throws an error if the directory creation fails.
     '''
     folderNames = ['config','notebook','data','script','output/model','output/script']
+    currentPath = os.getcwd()
     for folderName in folderNames:    
         try:
-            if not os.path.exists(folderName):
-                os.makedirs(folderName)
+            if not os.path.exists(folderName):                
+                os.makedirs(os.path.join(currentPath, folderName))
         except OSError:
             print ('Error: Creating directory. ' +  folderName)
--- tests/test_projectutils.py
+++ tests/test_projectutils.py
@@ -1,4 +1,18 @@
+import os
+import shutil
 from arcus.azureml.projectutils import initProjectStructure
+tmpTestPath = os.path.join(os.getcwd(), 'pytestTempFolder')
+
+def createTempTestPath():   
+   if os.path.exists(tmpTestPath):
+      shutil.rmtree(tmpTestPath)
+   os.mkdir(tmpTestPath)
 
 def test_initProjectStructure():
+   testRootPath = os.getcwd()
+   createTempTestPath()   
+   os.chdir(tmpTestPath)   
    initProjectStructure()
+   os.chdir(testRootPath)   
+   assert(len(next(os.walk(tmpTestPath))[1]) == 5)
+   shutil.rmtree(tmpTestPath)   
