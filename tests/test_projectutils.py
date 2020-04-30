96fb0c973a1953839c9e2708a86583af56d1935f
Sriram Narayanan(Codit) <sriram.narayanan@codit.eu>
2020-04-30 13:46:17 +0200

    Modified methodname and added extra parameter

--- tests/test_projectutils.py
+++ tests/test_projectutils.py
@@ -1,18 +1,29 @@
 import os
+import pathlib
 import shutil
-from arcus.azureml.projectutils import initProjectStructure
+import logging
+from arcus.azureml.projectutils import init_project_structure
 tmpTestPath = os.path.join(os.getcwd(), 'pytestTempFolder')
 
-def createTempTestPath():   
+def create_temp_testpath():
    if os.path.exists(tmpTestPath):
       shutil.rmtree(tmpTestPath)
    os.mkdir(tmpTestPath)
+   os.chdir(tmpTestPath)
 
-def test_initProjectStructure():
+def test_init_project_structure():
+   logger = logging.getLogger()
+   test_folderName = "sample"   
    testRootPath = os.getcwd()
-   createTempTestPath()   
-   os.chdir(tmpTestPath)   
-   initProjectStructure()
-   os.chdir(testRootPath)   
-   assert(len(next(os.walk(tmpTestPath))[1]) == 5)
-   shutil.rmtree(tmpTestPath)   
+   create_temp_testpath()
+   init_project_structure(test_folderName)
+   os.chdir(tmpTestPath)
+   countRootFolder = len(next(os.walk(tmpTestPath))[1])
+   logger.info(countRootFolder)
+   newSamplePath = os.path.join(tmpTestPath,test_folderName)
+   os.chdir(newSamplePath)
+   countFolders = len(next(os.walk(newSamplePath))[1])
+   logger.info(countFolders)
+   assert(countRootFolder==1 and countFolders==5)
+   os.chdir(testRootPath)
+   shutil.rmtree(tmpTestPath)
