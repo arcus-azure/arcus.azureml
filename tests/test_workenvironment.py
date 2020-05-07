60ed7df2b2f410c1d8a399277f7edbac139bd7eb
Sam <sam.vanhoutte@marchitec.be>
2020-05-07 13:47:22 +0200

    add azureml core in requirements & remove online test

--- tests/test_workenvironment.py
+++ tests/test_workenvironment.py
@@ -8,11 +8,6 @@ def test_disconnected_config():
     work_env = WorkEnvironment(connected = False)
     assert work_env.is_connected == False
 
-def test_load_default_connected_env():
-    work_env = WorkEnvironment(connected=True)
-    assert work_env != None
-    assert work_env.is_connected == True
-
 def test_load_connected_env_file():
     work_env = WorkEnvironment(connected=True)
     assert work_env != None
