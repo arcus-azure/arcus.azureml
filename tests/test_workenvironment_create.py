fe4d248a49067b3dd2b3a27af5a41d31ac14bded
Sam <sam.vanhoutte@marchitec.be>
2020-05-08 16:17:53 +0200

    disable test_aml_creation when running in devops

--- tests/test_workenvironment_create.py
+++ tests/test_workenvironment_create.py
@@ -1,13 +1,23 @@
 import arcus.azureml.environment.workenvironment as wenv
 
+def is_interactive():
+    # If the environment variable System_DefinitionId is not available, we run locally
+    return 'SYSTEM_DEFINITIONID' not in os.environ
+
+
+
 def test_local_creation():
     loc = wenv.WorkEnvironment.Create(False)
     assert type(loc) == wenv.LocalEnvironment
     assert loc.isvalid()
 
 def test_aml_creation():
-    loc = wenv.WorkEnvironment.Create(connected = True, config_file='.azureml/config.json')
-    assert type(loc) == wenv.AzureMLEnvironment
-    assert loc.isvalid()
+    if not is_interactive():
+        import pytest
+        pytest.skip('Test only runs when interactive mode enable')
+    
+    aml = wenv.WorkEnvironment.Create(connected = True, config_file='.azureml/config.json')
+    assert type(aml) == wenv.AzureMLEnvironment
+    assert aml.isvalid()
 
     
